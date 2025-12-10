620d6f44d6f3263c1187a6712746fb9f58832600
chandravan <chandravankumar5145@gmail.com>
2025-12-10 23:58:53 +0530

    database connection sucessful

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,39 @@
-import config from './config/config'
 import app from './app'
+import config from './config/config'
+//import { initRateLimiter } from './config/rateLimiter'
+import databaseService from './service/databaseService'
+import logger from './util/logger'
 
 const server = app.listen(config.PORT)
 
-;(() => {
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+;(async () => {
     try {
-        // eslint-disable-next-line no-console
-        console.info(`APPLICATION_STARTED`, {
+        // Database Connection
+        const connection = await databaseService.connect()
+        logger.info(`DATABASE_CONNECTION`, {
+            meta: {
+                CONNECTION_NAME: connection.name
+            }
+        })
+
+        //initRateLimiter(connection)
+        //logger.info(`RATE_LIMITER_INITIATED`)
+
+        logger.info(`APPLICATION_STARTED`, {
             meta: {
                 PORT: config.PORT,
                 SERVER_URL: config.SERVER_URL
             }
         })
     } catch (err) {
-        // eslint-disable-next-line no-console
-        console.error(`APPLICATION_ERROR`, { meta: err })
+        logger.error(`APPLICATION_ERROR`, { meta: err })
+
         server.close((error) => {
             if (error) {
-                // eslint-disable-next-line no-console
-                console.error(`APPLICATION_ERROR`, { meta: error })
+                logger.error(`APPLICATION_ERROR`, { meta: error })
             }
+
             process.exit(1)
         })
     }
